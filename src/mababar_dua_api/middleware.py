75e7f2e7ec013aa5d5641e6573851f4e1a2dba1a
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-05-05 13:28:35 +0600

    add type cvhecking in middleware

--- src/mababar_dua_api/middleware.py
+++ src/mababar_dua_api/middleware.py
@@ -7,7 +7,10 @@ from .request import Request
 
 async def run_middlewares(middlewares: list[Callable], request: Request) -> None:
     for mw in middlewares:
-        
+        if not isinstance(mw, (types.FunctionType, types.MethodType)) and not callable(
+            mw
+        ):
+            raise ValueError("Middlewares must be callable.")
         if inspect.iscoroutinefunction(mw):
             await mw(request)
         else:
