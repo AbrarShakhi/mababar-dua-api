f93f7cd4f8bf32cea68b58b75dcc97c06dd54ed4
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-05-05 13:42:33 +0600

    fox: error then returest 404

--- src/mababar_dua_api/routing.py
+++ src/mababar_dua_api/routing.py
@@ -47,16 +47,18 @@ class Router:
 
         for route in self.routes:
             parsed = parse(route.path, request.path)
+            if parsed is not None and route.method == request.method:
+                request.path_params = parsed.named
+                await run_middlewares(route.middlewares, request)
 
-        request.path_params = parsed.named
-        await run_middlewares(route.middlewares, request)
+                response = Response(status_code=404)
+                if inspect.iscoroutinefunction(route.handler):
+                    ret = await route.handler(request, response, **parsed.named)
+                else:
+                    ret = route.handler(request, response, **parsed.named)
+                return ret if isinstance(ret, Response) else response
 
-        response = Response(status_code=404)
-        if inspect.iscoroutinefunction(route.handler):
-            ret = await route.handler(request, response, **parsed.named)
-        else:
-            ret = route.handler(request, response, **parsed.named)
-        return ret if isinstance(ret, Response) else response
+        raise HTTPException(404, f"No route found for {request.method} {request.path}")
 
 
 def scan_class_for_routes(
